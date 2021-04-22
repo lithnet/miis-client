3682a5c73001b2e0aca662445d94f63b9d0e3f4f
Ryan Newington <ryan@lithiumblue.com>
2021-04-23 07:58:19 +1000

    Fixes an issue where delta objects were showing AttributeValue objects instead of AttributeValueChange objects

--- src/Lithnet.Miiserver.Client/Models/CSObject/AttributeChange.cs
+++ src/Lithnet.Miiserver.Client/Models/CSObject/AttributeChange.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 
 namespace Lithnet.Miiserver.Client
@@ -6,7 +7,7 @@ namespace Lithnet.Miiserver.Client
     /// <summary>
     /// Represents an attribute change and its value collection
     /// </summary>
-    public class AttributeChange : Attribute
+    public class AttributeChange : XmlObjectBase
     {
         internal AttributeChange(XmlNode node)
             : base(node)
@@ -18,17 +19,35 @@ namespace Lithnet.Miiserver.Client
         /// </summary>
         public AttributeOperation Operation => this.GetValue<AttributeOperation>("@operation");
 
+
+        public IReadOnlyList<AttributeValueChange> ValueChanges => this.GetReadOnlyObjectList<AttributeValueChange>("value", this.Type);
+      
+        /// <summary>
+        /// Gets the name of the attribute
+        /// </summary>
+        public string Name => this.GetValue<string>("@name");
+
+        /// <summary>
+        /// Gets the type of the attribute
+        /// </summary>
+        public AttributeType Type => this.GetValue<AttributeType>("@type");
+
+        /// <summary>
+        /// Gets a value that indicates if the attribute is multivalued or single-valued
+        /// </summary>
+        public bool Multivalued => this.GetValue<bool>("@multivalued");
+        
         /// <summary>Returns a string that represents the current object.</summary>
         /// <returns>A string that represents the current object.</returns>
         public override string ToString()
         {
             if (this.Operation == AttributeOperation.None)
             {
-                return $"{this.Name}:{this.Values.Select(t => t.ToString()).ToCommaSeparatedString()}";
+                return $"{this.Name}:{this.ValueChanges.Select(t => $"{t.Operation}:{t.Value}").ToCommaSeparatedString()}";
             }
             else
             {
-                return $"{this.Operation}:{this.Name}:{this.Values.Select(t => t.ToString()).ToCommaSeparatedString()}";
+                return $"{this.Operation}:{this.Name}:({this.ValueChanges.Select(t => $"{t.Operation}:{t.Value}").ToCommaSeparatedString()})";
             }
         }
     }
